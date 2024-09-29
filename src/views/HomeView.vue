d61fc600e181091b11cf5d0a2d87ed26d64ade87
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-09-29 19:16:45 +0200

    changed loader

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,7 +36,7 @@ export default {
 </script>
 
 <template>
-    <div v-if="loading" class="loading-screen">
+    <!-- <div v-if="loading" class="loading-screen">
         <div class="middle">
             <div class="bar bar1"></div>
             <div class="bar bar2"></div>
@@ -47,25 +47,36 @@ export default {
             <div class="bar bar7"></div>
             <div class="bar bar8"></div>
         </div>
+    </div> -->
+    <div>
+        <div v-if="loading" class="loader">
+            <div class="face">
+                <div class="circle"></div>
+            </div>
+            <div class="face">
+                <div class="circle"></div>
+            </div>
+        </div>
+        <div v-else class="content">
+            <Navbar />
+            <Hero />
+            <Skills />
+            <Works />
+            <Footer />
+        </div>
     </div>
-    <div v-else class="content">
-        <Navbar />
-        <Hero />
-        <Skills />
-        <Works />
-        <Footer />
-    </div>
+
 </template>
 
 <style scoped>
-.middle {
+/* .middle {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     position: absolute;
-}
+} */
 
-.bar {
+/* .bar {
     width: 10px;
     height: 70px;
     background: #fff;
@@ -73,7 +84,7 @@ export default {
     transform-origin: bottom center;
     border-top-right-radius: 20px;
     border-top-left-radius: 20px;
-    /* box-shadow: 5px 10px 20px inset rgba(255, 23, 25.2); */
+     box-shadow: 5px 10px 20px inset rgba(255, 23, 25.2); 
     animation: loader 1.2s linear infinite;
 }
 
@@ -112,7 +123,7 @@ export default {
 @keyframes loader {
     0% {
         transform: scaleY(0.1);
-        /* background: ; */
+        background: ;
     }
 
     50% {
@@ -125,20 +136,134 @@ export default {
         background: transparent;
     }
 }
+*/
 
-
-
-@keyframes slideInLeft {
+@keyframes elegantFadeIn {
     0% {
-        transform: translateX(-100%);
+        opacity: 0;
+    }
+
+    10% {
+        opacity: 0.1;
+    }
+
+    20% {
+        opacity: 0.2;
+    }
+
+    30% {
+        opacity: 0.3;
+    }
+
+    40% {
+        opacity: 0.4;
+    }
+
+    50% {
+        opacity: 0.5;
+    }
+
+    60% {
+        opacity: 0.6;
+    }
+
+    70% {
+        opacity: 0.7;
+    }
+
+    80% {
+        opacity: 0.8;
+    }
+
+    90% {
+        opacity: 0.9;
     }
 
     100% {
-        transform: translateX(0);
+        opacity: 1;
     }
 }
 
 .content {
-    animation: 1s ease-out 0s 1 slideInLeft;
+    animation: 1.5s ease-out 0s 1 elegantFadeIn;
+}
+
+
+/* ---------------SECOND LOADER---------------- */
+
+
+.loader {
+    width: 20em;
+    height: 20em;
+    font-size: 10px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+}
+
+.loader .face {
+    position: absolute;
+    border-radius: 50%;
+    border-style: solid;
+    animation: animate 3s linear infinite;
+}
+
+.loader .face:nth-child(1) {
+    width: 100%;
+    height: 100%;
+    color: #FFEE50;
+    border-color: currentColor transparent transparent currentColor;
+    border-width: 0.2em 0.2em 0em 0em;
+    --deg: -45deg;
+    animation-direction: normal;
+}
+
+.loader .face:nth-child(2) {
+    width: 70%;
+    height: 70%;
+    color: #FF4E47;
+    border-color: currentColor currentColor transparent transparent;
+    border-width: 0.2em 0em 0em 0.2em;
+    --deg: -135deg;
+    animation-direction: reverse;
+}
+
+.loader .face .circle {
+    position: absolute;
+    width: 50%;
+    height: 0.1em;
+    top: 50%;
+    left: 50%;
+    background-color: transparent;
+    transform: rotate(var(--deg));
+    transform-origin: left;
+}
+
+.loader .face .circle::before {
+    position: absolute;
+    top: -0.5em;
+    right: -0.5em;
+    content: '';
+    width: 1em;
+    height: 1em;
+    background-color: currentColor;
+    border-radius: 50%;
+    box-shadow: 0 0 2em,
+        0 0 4em,
+        0 0 6em,
+        0 0 8em,
+        0 0 10em,
+        0 0 0 0.5em rgba(255, 255, 0, 0.1);
+}
+
+@keyframes animate {
+    to {
+        transform: rotate(1turn);
+    }
 }
 </style>
