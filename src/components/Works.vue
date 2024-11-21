a42b0c082176c9f5f02dc885ff93019fd87055d6
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-11-21 21:49:04 +0100

    test

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -72,26 +72,26 @@
                 </div>
             </div>
         </div>
-        <div class="slider" reverse="true" style="
+        <div class="slider" style="
         --width: 250px;
         --height: 220px;
         --quantity: 5;
         ">
-            <!-- <div class="list">
+            <div class="list">
                 <div class="item" style="--position: 1"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt=""></div>
                 <div class=" item" style="--position: 2"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt=""></div>
                 <div class="item" style="--position: 3"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt="">
                 </div>
                 <div class="item" style="--position: 4"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt=""></div>
-                <div class="item" style="--position: 5"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt=""></div> -->
-            <!-- <div class="item" style="--position: 6"><img src="./icons/DiscordRicca.png" alt=""></div>
+                <div class="item" style="--position: 5"><img src="/dist/assets/dungeon-work-BRbrBqWZ.png" alt=""></div>
+                <div class="item" style="--position: 6"><img src="./icons/DiscordRicca.png" alt=""></div>
                 <div class="item" style="--position: 7"><img src="./icons/Avada.png" alt=""></div>
                 <div class="item" style="--position: 8"><img src="./icons/Ricky Sfondi_Tavola disegno 1-02.png" alt="">
                 </div>
                 <div class="item" style="--position: 9"><img src="./icons/dungeon-work.png" alt=""></div>
-                <div class="item" style="--position: 10"><img src="./icons/marvel-work.png" alt=""></div> -->
-            <!-- </div> -->
-            <div class="list">
+                <div class="item" style="--position: 10"><img src="./icons/marvel-work.png" alt=""></div>
+            </div>
+            <!-- <div class="list">
                 <div class="item" style="--position: 1">
                     <p>BookaVacay</p>
                 </div>
@@ -122,7 +122,7 @@
                 <div class="item" style="--position: 10">
                     <p>Discord</p>
                 </div>
-            </div>
+            </div> -->
         </div>
         <div class="space-div"></div>
     </div>
